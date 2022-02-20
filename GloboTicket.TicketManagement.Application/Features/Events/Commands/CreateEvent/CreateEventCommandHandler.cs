78706fbb44b94e488d19df61d584c424cac30b44
NourNafea <nour68.nafea@gmail.com>
2022-02-20 16:49:32 +0200

    Add Startup.cs

--- GloboTicket.TicketManagement.Application/Features/Events/Commands/CreateEvent/CreateEventCommandHandler.cs
+++ GloboTicket.TicketManagement.Application/Features/Events/Commands/CreateEvent/CreateEventCommandHandler.cs
@@ -31,18 +31,18 @@ public class CreateEventCommandHandler : IRequestHandler<CreateEventCommand, Gui
 
         @event = await _eventRepository.AddAsync(@event);
 
-        //Sending email notification to admin address
-        // var email = new Email() { To = "gill@snowball.be", Body = $"A new event was created: {request}", Subject = "A new event was created" };
-
-        // try
-        // {
-        //     await _emailService.SendEmail(email);
-        // }
-        // catch (Exception ex)
-        // {
-        //     //this shouldn't stop the API from doing else so this can be logged
-        //     _logger.LogError($"Mailing about event {@event.EventId} failed due to an error with the mail service: {ex.Message}");
-        // }
+        // Sending email notification to admin address
+        //  var email = new Email() { To = "gill@snowball.be", Body = $"A new event was created: {request}", Subject = "A new event was created" };
+        //
+        //  try
+        //  {
+        //      await _emailService.SendEmail(email);
+        //  }
+        //  catch (Exception ex)
+        //  {
+        //      //this shouldn't stop the API from doing else so this can be logged
+        //      _logger.LogError($"Mailing about event {@event.EventId} failed due to an error with the mail service: {ex.Message}");
+        //  }
 
         return @event.EventId;
     }
--- loboTicket.TicketManagement.Api/Program.cs
+++ loboTicket.TicketManagement.Api/Program.cs
@@ -1,6 +1,37 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
 
-app.Run();
+namespace GloboTicket.TicketManagement.Api
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var config = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            // Log.Logger = new LoggerConfiguration()
+            //     .ReadFrom.Configuration(config)
+            //     .WriteTo.File("Logs/log-.txt", rollingInterval: RollingInterval.Day) 
+            //     .CreateLogger();
+
+            var host = CreateHostBuilder(args).Build();
+            
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                var loggerFactory = services.GetRequiredService<ILoggerFactory>();
+            }
+
+            host.Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                // .UseSerilog()
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
