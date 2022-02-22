a35ec3dfb5ead5838c9522f3f632072c38b55ca9
NourNafea <nour68.nafea@gmail.com>
2022-02-22 16:18:17 +0200

    Add Unit Test

--- GloboTicket.TicketManagement.Application/Features/Categories/Queries/GetCategoriesList/GetCategoriesListQueryHandler.cs
+++ GloboTicket.TicketManagement.Application/Features/Categories/Queries/GetCategoriesList/GetCategoriesListQueryHandler.cs
@@ -1,21 +1,20 @@
-﻿using System.Collections.Generic;
-using System.Linq;
-using System.Threading;
-using System.Threading.Tasks;
-using AutoMapper;
+﻿using AutoMapper;
 using GloboTicket.TicketManagement.Application.Contracts.Persistence;
 using GloboTicket.TicketManagement.Domain.Entities;
 using MediatR;
-
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace GloboTicket.TicketManagement.Application.Features.Categories.Queries.GetCategoriesList
 {
     public class GetCategoriesListQueryHandler : IRequestHandler<GetCategoriesListQuery, List<CategoryListVm>>
     {
-        private readonly ICategoryRepository _categoryRepository;
+        private readonly IAsyncRepository<Category> _categoryRepository;
         private readonly IMapper _mapper;
 
-        public GetCategoriesListQueryHandler(IMapper mapper, ICategoryRepository categoryRepository)
+        public GetCategoriesListQueryHandler(IMapper mapper, IAsyncRepository<Category> categoryRepository)
         {
             _mapper = mapper;
             _categoryRepository = categoryRepository;
